855eef7970ce081a484781484867a99a0f8ac248
Lillious <logan.brown@tylertech.com>
2025-11-06 13:27:11 -0500

    Update mysql database setup

--- src/utility/database_setup.ts
+++ src/utility/database_setup.ts
@@ -329,8 +329,8 @@ const createQuestLogTable = async () => {
     CREATE TABLE IF NOT EXISTS quest_log (
       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
       username VARCHAR(255) UNIQUE NOT NULL,
-      completed_quests TEXT NOT NULL,
-      incomplete_quests TEXT NOT NULL
+      completed_quests TEXT NOT NULL default '0',
+      incomplete_quests TEXT NOT NULL default '0'
     )
   `;
   await query(sql);
